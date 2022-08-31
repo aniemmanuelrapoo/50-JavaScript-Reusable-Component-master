a58ae11f35e463c4ad0514f2ddfcc1249a454c6b
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-09-01 00:59:02 +0100

    added a new project

---
+++ faqs/main.js
@@ -0,0 +1,16 @@
+//show/hide faq answers
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open')
+
+    //change icon to minus
+    const icon = faq.querySelector('.faq__icon i')
+    if(icon.className === 'uil uil-plus'){
+      icon.className = "uil uil-minus"
+    }else{
+      icon.className = "uil uil-plus"
+    }
+  })
+})
